24ebe83deb72ee7bf099407b1afe8fca849257ea
Rachit <rachit.aryal@ranenetwork.com>
2022-03-24 19:32:21 +0545

    added crud handler func

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type Movie struct {
 	ID string `json: "id"`
 	Isbn string `json: "isbn"`
@@ -13,7 +21,30 @@ type Director struct {
 	LastName string `json: "lastName"`
 }
 
+func getMovies(res http.ResponseWriter, req *http.Request){}
+
+func getMovie(res http.ResponseWriter, req *http.Request){}
+
+func createMovie(res http.ResponseWriter, req *http.Request){}
+
+func updateMovie(res http.ResponseWriter, req *http.Request){}
+
+func deleteMovie(res http.ResponseWriter, req *http.Request){}
+
+var movies []Movie
+
 func main(){
 	// main function
+
+	router := mux.NewRouter()
+	router.HandleFunc("/movies", getMovies).Methods("GET")
+	router.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	router.HandleFunc("/movies", createMovie).Methods("POST")
+	router.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	router.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	port := ":8000"
+	fmt.Println("Starting Server @", port )
+	log.Fatal(http.ListenAndServe(port, router))
 
 }
